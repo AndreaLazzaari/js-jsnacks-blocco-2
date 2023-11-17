4ef3ad3b63f8eede01e30e921e4dff5bb088235a
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-17 11:28:44 +0100

    inserita condizione while

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,6 +1,12 @@
 // Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
 // Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
+let numeroUtente = prompt('inserisci un numero');
 
-const array = []
+const array = numeroUtente.split('');
 
+let somma = 0
+
+while(somma += parseInt(array[i])){
+    
+}
