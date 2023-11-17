bcffcaaaf4bcb0daa7f74883b5d30f73b1e65304
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-17 12:03:02 +0100

    impostazione snack-2

---
+++ sanck-2/script.js
@@ -0,0 +1,10 @@
+// Generatore di "nomi cognomi" casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e
+// da questi due array distinti vuole generare una falsa lista di invitati con nome e cognome.
+
+const nomiArray = ['andrea', 'luca', 'gina', 'inetta'];
+const cognomiArray = ['lori', 'rossi', 'pumi', 'razo'];
+
+let somma = 0
+
+const falsoArray = 
+
--- snack-1/script.js
+++ snack-1/script.js
@@ -1,15 +1,18 @@
 // Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
 // Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
-let numeroUtente = prompt('inserisci un numero');
 
-const array = numeroUtente.split('');
+const array = [];
 
 let somma = 0
 
 while(somma < 50){
-    prompt('inserisci un altro numero')
-    somma = somma + array
+    let numeroUtente = parseInt(prompt('inserisci un numero'));
+    if ( !isNaN(numeroUtente) ){
+    somma = somma + numeroUtente;
+    array.push(numeroUtente)
+    }
+
 }
 
-console.log(array)
+console.log(array, somma)
