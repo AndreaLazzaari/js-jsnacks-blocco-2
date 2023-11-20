a1b91cd509321a55fcbf6699f6af1c9debd4c30b
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-20 12:06:33 +0100

    snack completato

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,17 +1,25 @@
 // Dare la possibilità di inserire due parole. Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. Se hanno 
 // la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
+let parolaNumeroUno = prompt('inserisci una parola')
+let parolaNumeroDue = prompt('inserisci una parola')
 
-function lunghezzaParole(parolaNumeroUno, parolaNumeroDue) {
-    let parolaNumeroUno = prompt('inserisci una parola')
-    let parolaNumeroDue = prompt('inserisci una parola')
-    if (parolaNumeroUno.length === parolaNumeroDue.length){
-        console.log(parolaNumeroUno, parolaNumeroDue)
-    } else if (parolaNumeroUno.length < parolaNumeroDue.length){
-        console.log(parolaNumeroDue)
-    } else {
-        console.log (parolaNumeroUno)
-    }
-
-    return 
+if ( lunghezzaParole(parolaNumeroUno, parolaNumeroDue) ){
+   console.log(parolaNumeroUno, parolaNumeroDue)
+} else if (parolaNumeroUno.length < parolaNumeroDue.length){
+   console.log(parolaNumeroDue)
+} else {
+   console.log (parolaNumeroUno)
 }
+
+function lunghezzaParole(wordUno, wordDue) {
+   if (wordUno.length === wordDue.length){
+    return true;
+   } else {
+    return false;
+   }
+  
+
+    
+}
+
