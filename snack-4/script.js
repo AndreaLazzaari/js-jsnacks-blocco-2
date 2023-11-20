129391e5bf93e5e2a7478c8da12b8621a40e1bca
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-20 12:11:26 +0100

    creati snack-4

---
+++ snack-4/script.js
@@ -0,0 +1,12 @@
+// Crea una funzione che accetta due numeri, se il primo e' divisibile per il secondo, 
+// allora ritorna vero, altrimenti ritorna falso. Micro bonus: se sono uguali ritorna il numero.
+
+function comparaDueNumeri(numeroUno, numeroDue) {
+   if (numeroUno / numeroDue === 0){
+      return true
+   } else {
+      return false
+   }
+   
+}
+
