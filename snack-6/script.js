fb944f7e2a07b20d8b8403f245f56244414c8cc7
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-23 12:20:54 +0100

    inizializzazione snack-7 (cronometro)

--- snack-6/script.js
+++ snack-6/script.js
@@ -1,12 +1,17 @@
 // Chiedo all'utente un numero e genero attraverso una funzione un numero di 
 // quadrati equivalente al numero inserito dall'utente nel DOM, in qualsiasi forma. 
 
-const numeroUtente = 5
+const numeroUtente = parseInt(prompt('inserisci un numero'))
 
-function creazioneQuadrati(numero) {
-   const quadrato = document.createElement ('section.article')
-   console.log(quadrato)
-   quadrato.appendChild('section')
+const wrapper = document.querySelector('article.square')
+
+for (let index = 0; index < numeroUtente; index++){
+  wrapper.appendChild(creazioneQuadrati());
 }
 
-let risultato = creazioneQuadrati(2)
+function creazioneQuadrati() {
+   const elementoDiv = document.createElement ('article')
+   elementoDiv.classList.add('square')
+   return elementoDiv
+}
+
---
+++ snack-7/script.js
@@ -0,0 +1,11 @@
+
+
+// Cliccando su un pulsante, viene avviato un cronometro. Per fermare il cronometro, bisogna cliccare su un secondo pulsante
+
+document.querySelector('#inizio-timer').addEventListener('click', function(){
+  setTimeout(() => {
+    
+  }, document.querySelector('fine-timer').addEventListener('click', function(){
+    
+  }));
+})
