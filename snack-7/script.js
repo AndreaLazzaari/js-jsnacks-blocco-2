5444e0cbc4decbe28c205ee0d6d0893386adb4f1
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-24 12:31:27 +0100

    prova per vedere se creo l'array

--- snack-7/script.js
+++ snack-7/script.js
@@ -1,11 +1,5 @@
 
+const bottoneInizio = document.getElementById('inizio-timer');
+const bottoneFine = document.getElementById('fine-timer');
+// const bottoneInizio = document.getElementById('inizio-timer');
 
-// Cliccando su un pulsante, viene avviato un cronometro. Per fermare il cronometro, bisogna cliccare su un secondo pulsante
-
-document.querySelector('#inizio-timer').addEventListener('click', function(){
-  setTimeout(() => {
-    
-  }, document.querySelector('fine-timer').addEventListener('click', function(){
-    
-  }));
-})
---
+++ snack-8/script.js
@@ -0,0 +1,14 @@
+// Crea 10 oggetti che rappresentano una zucchina,
+// indicandone per ognuno varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine.
+
+
+const listaZucchine = [
+    'nera' = {
+        varieta : 'nera',
+        peso : 3,
+        lunghezza : 6
+    }
+
+
+
+];
